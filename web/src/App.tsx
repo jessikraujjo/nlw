f202996104a42c014c19db82b02542049543e111
jessikraujjo <jessikraujjo@gmail.com>
2020-06-07 16:18:10 -0300

    App

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+import Routes from './routes';
+
+/*jsx: sintaxe do xml dentro do javascript*/
+
+function App() {
+  return (
+   <Routes/>
+    //div#app>ul>li*5
+  );
+}
+
+export default App;
---
+++ web/src/index.tsx
@@ -0,0 +1,13 @@
+//import React from 'react';
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+import App from './App';
+
+ReactDOM.render( //mostra na tela o APP
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
